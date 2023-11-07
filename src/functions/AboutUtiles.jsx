946578b6ee7df3321d02fad83e97ea3fd48238a7
Sik <contact@sikelio.wtf>
2023-11-07 20:18:30 +0100

    Converted About in class

--- src/functions/AboutUtiles.jsx
+++
@@ -1,9 +0,0 @@
-// Dependencies
-import axios from "axios";
-
-export const fetchAuthors = async (setAuthors) => {
-    try {
-        const data = await axios.get('https://api.github.com/repos/sikelio/plexmanager/contributors');
-        setAuthors(data.data);
-    } catch (e) {}
-}
