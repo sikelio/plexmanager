0d51b38d77c459868f0f9a61b41bf884865c83ec
Sik <contact@sikelio.wtf>
2023-11-08 22:11:58 +0100

    Converted ScheduledTasks in class

--- src/screens/ScheduledTasks.jsx
+++ src/screens/ScheduledTasks.jsx
@@ -1,39 +1,59 @@
-// Components
-import { Text, ScrollView, View } from "react-native";
-import { Card } from "@rneui/themed";
-// Style
-import style from "../style/ScheduledTasksStyle";
-const ScheduledTasks = ({ route }) => {
-    const { scheduledTasks } = route.params;
+import React from 'react';
+import { Text, ScrollView, View, StyleSheet } from 'react-native';
+import { Card } from '@rneui/themed';
+import { useNavigation } from "@react-navigation/native";
 
-    return (
-        <ScrollView>
-            {scheduledTasks.map((task, index) => {
-                return (
-                    <Card
-                        key={ index }
-                    >
-                        <Card.Title>{ task.name }</Card.Title>
-                        <Card.Divider />
-                        <View>
-                            <Text style={ [style.textColor] }>
-                                <Text style={ [style.textLabel] }>Description : </Text>
-                                { task.description }
-                            </Text>
-                            <Text style={ [style.textColor] }>
-                                <Text style={ [style.textLabel] }>Schedule randomized : </Text>
-                                { task.scheduleRandomized.toString() }
-                            </Text>
-                            <Text style={ [style.textColor] }>
-                                <Text style={ [style.textLabel] }>Interval : </Text>
-                                { task.interval }
-                            </Text>
-                        </View>
-                    </Card>
-                );
-            })}
-        </ScrollView>
-    );
+class ScheduledTasks extends React.Component {
+    localStyle = StyleSheet.create({
+        textColor: {
+            color: '#000'
+        },
+        textLabel: {
+            fontWeight: 'bold'
+        }
+    });
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            scheduledTasks: this.props.route.params.scheduledTasks
+        };
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                {this.state.scheduledTasks.map((task, index) => {
+                    return (
+                        <Card
+                            key={index}
+                        >
+                            <Card.Title>{task.name}</Card.Title>
+                            <Card.Divider />
+                            <View>
+                                <Text style={this.localStyle.textColor}>
+                                    <Text style={this.localStyle.textLabel}>Description : </Text>
+                                    {task.description ? task.description : 'none'}
+                                </Text>
+                                <Text style={this.localStyle.textColor}>
+                                    <Text style={this.localStyle.textLabel}>Schedule randomized : </Text>
+                                    {task.scheduleRandomized.toString()}
+                                </Text>
+                                <Text style={this.localStyle.textColor}>
+                                    <Text style={this.localStyle.textLabel}>Interval : </Text>
+                                    {task.interval}
+                                </Text>
+                            </View>
+                        </Card>
+                    );
+                })}
+            </ScrollView>
+        );
+    }
 }
 
-export default ScheduledTasks;
+export default (props) => {
+    const navigation = useNavigation();
+    return <ScheduledTasks {...props} navigation={navigation} />;
+};
