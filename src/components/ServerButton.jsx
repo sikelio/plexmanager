025163d58b245813198443d52e76234b19a75f02
Sik <contact@sikelio.wtf>
2023-11-11 12:23:47 +0100

    Converted ServerButton in class

--- src/components/ServerButton.jsx
+++ src/components/ServerButton.jsx
@@ -1,29 +1,46 @@
-// Dependencies
 import React from 'react';
-// Components
+import { StyleSheet } from 'react-native';
 import { Button } from '@rneui/themed';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-// Styles
-import style from '../style/ServerStyle';
 
-const ServerButton = ({iconName, iconColor, backgroundColor, btnTitle, onPress }) => {
-    return (
-        <Button
-            icon={
-                <Icon
-                    name={ iconName }
-                    color={ iconColor }
-                    size={ 16 }
-                    style={ [style.cardBtn] }
-                />
-            }
-            buttonStyle={{
-                backgroundColor: backgroundColor
-            }}
-            title={ btnTitle }
-            onPress={ onPress }
-        />
-    );
+class ServerButton extends React.Component {
+    localStyle = StyleSheet.create({
+        cardBtn: {
+            marginRight: 10
+        }
+    });
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            iconName: this.props.iconName,
+            iconColor: this.props.iconColor,
+            backgroundColor: this.props.backgroundColor,
+            btnTitle: this.props.btnTitle,
+            onPress: this.props.onPress
+        };
+    }
+
+    render() {
+        return (
+            <Button
+                icon={
+                    <Icon
+                        name={this.state.iconName}
+                        color={this.state.iconColor}
+                        size={16}
+                        style={this.localStyle.cardBtn}
+                    />
+                }
+                buttonStyle={{
+                    backgroundColor: this.state.backgroundColor
+                }}
+                title={this.state.btnTitle}
+                onPress={this.state.onPress}
+            />
+        );
+    }
 }
 
 export default ServerButton;
